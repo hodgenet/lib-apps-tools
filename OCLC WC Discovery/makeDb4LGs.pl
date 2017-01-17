fb300d66bf7e63c8cedff4e87c274a3518419deb
hodgenet <thom@hodgnet.com>
2017-01-17 14:43:39 +0400

    Remove debugging, fix headers, etc

--- OCLC WC Discovery/makeDb4LGs.pl
+++ OCLC WC Discovery/makeDb4LGs.pl
@@ -1,8 +1,7 @@
 #!/usr/bin/perl 
-##-w
 
 use strict;
-use lib '/path/to/OCLC Auth Modules/';
+use lib '/home/apache/OCLCMods/';
 use OCLCAuth qw(:All);
 use OCLCCred;
 use LWP::UserAgent;
@@ -10,89 +9,14 @@ use Data::Dumper;
 use XML::Parser;                                                        #       use Parser to read the XML file,
 use XML::SimpleObject;                                                  #       Simple Object to handle the actual records
 
-## This script is builds two files: 1) an xml dublin core file containing records of your WC Knowledgebase Collections, and 2) a delimited
-## file suitable for loading into libguides as a resource file.  It does this by using the License Manager Beta API and doing a full list of 
-## of all licenses, parsing them to get the descriptions and the associated collections id, then retrieving each collection record, and 
-## writing records for it to the delimited and xml files.  We use the XML file with an XSL sheet to build website database pages,
-## though we're looking at just using the Libguide A-Z pages
-
-
-########################		 subroutine to normalize a title		################################
-
-sub normalizeTitle {
-
-	my $ntitle = $_[0];
-	$ntitle =~ s/[\s\.:\/]$//g;		#	Remove trailing punctuation
-	$ntitle =~ s/[\s]$//g;
-	$ntitle =~ s/^The //;			#	Remove initial article
-	$ntitle =~ s/^An //;			#	Remove initial article
-	$ntitle =~ s/^A //;			#	Remove initial article
-	$ntitle = ucfirst($ntitle);
-
-return $ntitle;
-}
-
-
-########################		 subroutine to normalize a websubject		################################
-
-sub normalizeWebSubject {
-
-	my $nwsubject = $_[0];
-	$nwsubject =~ s/&/&amp;/g;		#	entitize any ampersands
-	$nwsubject =~ s/\"//g;			#	entitize any quotes
-	$nwsubject =~ s/\.$//g;			#	Remove trailing period
-
-return $nwsubject;
-}
-
-
-########################		 subroutine to normalize a description		################################
-
-sub normalizeDescription {
-
-	my $ndescription = $_[0];
-#	$ndescription =~ s/&/&amp;/g;		#	entitize any ampersands
-#	$ndescription =~ s/>/&gt;/g;		#	convert any arrow brackets
-#	$ndescription =~ s/</&lt;/g;		#	convert any arrow brackets
-	$ndescription =~ s/&quot /&quot;/g;
-	$ndescription =~ s/\{u00AE\}/&#169;/g;		#	convert any arrow brackets
-	$ndescription =~ s/\{u2122\}/&#8482;/g;		#	convert any arrow brackets
-	$ndescription =~ s/\{u2019\}/\'/g;		#	convert any arrow brackets
-	$ndescription =~ s/\{u2014\}/&#8212;/g;		#	convert any arrow brackets
-	$ndescription =~ s/\{u201C\}/&#x34;/g;		#	convert any arrow brackets
-	$ndescription =~ s/\{u201D\}/&#34;/g;		#	convert any arrow brackets
-	$ndescription =~ s/\{u00E6\}/&#230;/g;		#	convert any arrow brackets
-
-	$ndescription = ucfirst($ndescription);
-
-return $ndescription;
-}
-
-
-########################		 subroutine to normalize an identifier		################################
-
-sub normalizeIdentifier {
-
-	my $nidentifier = $_[0];
-	$nidentifier =~ s/&/&amp;/g;		#	entitize any ampersands
-	$nidentifier =~ s/^\s//g;
-	$nidentifier =~ s/\s$//g;
-
-return $nidentifier;
-}
+## This script is builds  a delimited file suitable for loading into libguides as a resource file.  It does this by using the License Manager 
+## Beta API and doing a full list of of all licenses, parsing them to get the descriptions and the associated collections id, then retrieving 
+## each collection record, and writing records for it to the delimited file.  We plan to use the Libguide A-Z pages to replace the XML based pages
 
 
 ############   get/make the value for totalResults from the current result ############
 
 sub parseLicenses {
-
-open (OUT, ">> ./licCollOut.txt") or die $!;
-binmode OUT, ":utf8";
-
-open (LIBG, ">> ./libguideFile.txt") or die $!;
-binmode LIBG, ":utf8";
-
-print LIBG "vendor\tname\turl\tenable_proxy\tdescription\tcontent_id\n";
 
 	my $apiRecord = ${$_[0]}; 
 	my $totalResults = 0; my $theseItemsPerPage;
@@ -106,11 +30,10 @@ foreach my $entry ( $xso->child('feed')->child('entry') )  {
 
 	my $licID = $entry->child('content')->child('license')->child('id')->value;
 	my $licName = $entry->child('content')->child('license')->child('name')->value;
-print OUT "License Name: \t$licName\n";
+
 	my $licDescription = '';
 	if ( defined $entry->child('content')->child('license')->child('description') ) { 
 		$licDescription = $entry->child('content')->child('license')->child('description')->value;
-print OUT "$licDescription\n\n";
 	}
 
 	foreach my $collection ( $entry->child('content')->child('license')->child('collections')->child('collection') ) {
@@ -119,26 +42,16 @@ print OUT "$licDescription\n\n";
 		if ( defined $collection->child('name') ) { $thisCollectionName = $collection->child('name')->value; } else { $thisCollectionName = 'No Name'; }
 		if ( defined $collection->child('provider') ) { $thisCollectionProvider = $collection->child('provider')->value; }
 
-print OUT "\tCollection Name :\t$thisCollectionName\t";
-print "\tCollection ID :\t$thisCollectionID\t";
-print "\tCollection Name :\t$thisCollectionName\t";
-
 		## TODO Store these in a data structure for collections
 		my $thisXMLCollectionRecord = getCollectionRecord(\$thisCollectionID,\$thisCollectionName);	## call getCollectionRecord
 		my $thisCollectionUrl = ''; my $summary = ''; my $content_id = ''; my $owner_institution = ''; my $source_institution = ''; 
 		my $staff_notes = ''; my $public_notes = ''; my $provider_name = ''; my $localstem = '';
 		($thisCollectionUrl,$summary,$content_id,$public_notes,$staff_notes,$owner_institution,$source_institution,$provider_name,$localstem) = parseCollectionRecord(\$thisXMLCollectionRecord);
-print OUT "URL : $thisCollectionUrl\n";
-print "Content ID in parseLicense: $content_id\n";
+
 		my $libGDesc = '';
-		if ( $summary ne '' ) { print OUT "\tCollection Summary: \t$summary\n\n"; $libGDesc = $summary; } else { print OUT "\n"; $libGDesc = $licDescription; }
-		if ( $content_id ne '' ) { print OUT "\tContentID: \t$content_id\n"; } else { print OUT "\n"; }
-		if ( $owner_institution ne '' ) { print OUT "\tOwner Inst: \t$owner_institution\t"; } else { print OUT "\t"; }
-		if ( $source_institution ne '' ) { print OUT "\tSource Inst: \t$source_institution\n"; } else { print OUT "\n"; }
-		if ( $staff_notes ne '' ) { print OUT "\tStaff Notes: \t$staff_notes\n"; } else { print OUT "\n"; }
-		if ( $public_notes ne '' ) { print OUT "\tPublic Notes: \t$public_notes\n\n"; } else { print OUT "\n"; }
+		if ( $summary ne '' ) { $libGDesc = $summary; } else { $libGDesc = $licDescription; }
 		if ( $localstem eq 'false' ) { $localstem = 0; } else { $localstem = 1; }
-print LIBG "$provider_name\t$thisCollectionName\t$thisCollectionUrl\t$localstem\t$libGDesc\t$content_id\n";
+		print LIBG "$provider_name\t$thisCollectionName\t$thisCollectionUrl\t$localstem\t$libGDesc\t$content_id\n";
 	}
 }
 
@@ -158,33 +71,26 @@ my $parser = new XML::Parser(ErrorContext => 1,                       #       Cr
                            Style => "Tree");
 my $xso = XML::SimpleObject->new($parser->parse("$collectionXMLRecord"));      #       Create the object for the parsed records and read
 
-#warn Dumper ($xso);
-
 my $collectionUrl = '';
 foreach my $link ( $xso->child('entry')->child('link') )  {
 	my %attributes = $link->attributes;
 	if ($attributes{'rel'} eq 'via' ) { $collectionUrl = $attributes{'href'};
-#	if ( $link->attribute eq "rel"  && $link->attribute('rel')->value eq "via") {
-#		$collectionUrl = $link->attribute('href')->value;
 	}
 
 }
-print " URL in parseCollection: $collectionUrl\n";
-my $summary = ''; my $content_id = '';
+
+my $summary = ''; 
 if ( defined $xso->child('entry')->child('summary') ) {
 	$summary = $xso->child('entry')->child('summary')->value;
-	if ( $summary =~ /content_id:([\d]{6,10})/ ) {
-		$content_id = $1;
-		$summary =~ s/content_id:$1//;
-	}
 }
-print "\n";
-print "CollectionID in parseCollection : $content_id\n";
-print "Summary in parseCollection : $summary\n";
-print "\n";
-my $staff_notes = '';
+
+my $staff_notes = ''; my $content_id = '';
 if ( defined $xso->child('entry')->child('kb:collection_staff_notes') ) {
 	$staff_notes = $xso->child('entry')->child('kb:collection_staff_notes')->value;
+	if ( $staff_notes =~ /content_id:([\d]{6,10})/ ) {
+		$content_id = $1;
+		$staff_notes =~ s/content_id:$1//;
+	}
 }
 
 my $public_notes = '';
@@ -212,13 +118,6 @@ if ( defined $xso->child('entry')->child('kb:localstem') ) {
 	$localstem = $xso->child('entry')->child('kb:localstem')->value;
 }
 
-
-#my $collectionTitle = $xso->child('entry')->child('title')->value;
-#my $collectionID = $xso->child('entry')->child('kb:collection_uid')->value;
-
-#$collectionRecord{$collectionID} = $collectionUrl;
-
-#return (\%collectionRecord);
 return ($collectionUrl,$summary,$content_id,$public_notes,$staff_notes,$owner_institution,$source_institution,$provider_name,$localstem);
 
 }
@@ -230,16 +129,18 @@ sub getCollectionRecord {
 
 my $collectionID = ${$_[0]}; my $collectionName = ${$_[1]}; 
 
-my $parmfile = '/path/to/param/file.txt';				## module will need to accept parameters - key, secret, principalID, registryID, principalIDNS.  Need to be stored securely
-##my ($key,$secret,$principalID,$principalIDNS,$registryID,$regionHost) = &getAPIParams(\$parmfile);
+my $parmfile = '/home/apache/.params/APIParms.txt';				## module will need to accept parameters - key, secret, principalID, registryID, principalIDNS.  Need to be stored securely
+
 my %instParams = %{get_ip(\$parmfile)};
 my $wskey = $instParams{'wskey'};
 my $institutionID = $instParams{'contextInstitutionId'};
 
 my $apiURL = 'https://worldcat.org/webservices/kb/rest/collections/'.$collectionID.','.$institutionID;		## for KB Manager API
-#my $apiURL = 'https://worldcat.org/webservices/kb/rest/collections/seach?intitution_uid='.$institutionID.'&collection_uid='.$collectionID;		## for KB Manager API
+#my $apiURL = 'https://worldcat.org/webservices/kb/rest/collections/search';  # for doing a search, need to modify routine to find the entry if you use this
 
-my %queryParams = ('wskey'=>"$wskey");						## for APIs that use WSKEY query paramete
+my %queryParams = ('wskey'=>"$wskey");						## for APIs that use WSKEY query parameter
+#my %queryParams = ('institution_uid'=>"$institutionID",'collection_uid'=>"$collectionID",'wskey'=>"$wskey");  # for doing a search, need to modify routine to find the entry if you use this
+
 my $ua = LWP::UserAgent->new;
 $ua->timeout(20);
 $ua->protocols_allowed( ['http', 'https'] );
@@ -248,13 +149,7 @@ my $requestURL = make_hru(\$apiURL,\%queryParams);		## Call makeOCLCRequestURL f
 my $response = $ua->get($requestURL);						## for  ??
 my $apiRecord = $response->content;
 
-if (defined $apiRecord) {
-print "\n";
-##	warn Dumper($apiRecord);
-}
-
 return ($apiRecord);
-
 }
 
 
@@ -262,7 +157,7 @@ return ($apiRecord);
 
 
 ## Get the record data
-my $parmfile = '/path/to/param/file.txt';				## module will need to accept parameters - wskey, secret, principalID, principalIDNS
+my $parmfile = '/home/apache/.params/LicAPIParms.txt';				## module will need to accept parameters - wskey, secret, principalID, principalIDNS
 										## auth registryID, context registryID, datacenter
 my %instParams = %{get_ip(\$parmfile)};
 
@@ -275,9 +170,12 @@ my $licenseStatusFilter = 'current';
 my $apiURL = 'https://'.$instParams{'authenticatingInstitutionId'}.'.share.worldcat.org/license-manager/license/list';			## for License Manager API
 my $APIService = 'WMS_LMAN';
 my $accessToken = request_at_ccg(\$APIService,\%instParams);			## TODO add some code to keep track of expiration
-##warn Dumper ($accessToken);							## Debugging
 my $atAuthHeader = make_atah(\$accessToken);
-##print "Access Token Header : $atAuthHeader\n";					## Debugging
+
+open (LIBG, ">> ./libguideFile.txt") or die $!;
+binmode LIBG, ":utf8";
+
+print LIBG "vendor\tname\turl\tenable_proxy\tdescription\tcontent_id\n";
 
 ############## Set the request ###################
 
@@ -294,14 +192,12 @@ while ( $startIndex < $totalResults) {					## when total results falls below ite
 print "Request URL in Main : $requestURL\n";					## Debugging
 
 	my $response = $ua->get($requestURL, 'Authorization'=>$atAuthHeader);	## for APIs that send an authHeader.
-##warn Dumper($response);							## debugging : uncomment if you want to see the request/response
 
 	my $apiRecord = $response->content;
-warn Dumper($apiRecord);							## debugging: dumps results to screen. Modify parseApiRecord to output entries to file
 	
 	($totalResults) = parseLicenses(\$apiRecord);
-## print "Total results : $totalResults\n";
+
 	$startIndex = $startIndex + $itemsPerPage;				## increment the start index for next batch
 }
 
-
+close LIBG;
